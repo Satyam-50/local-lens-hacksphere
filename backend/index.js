e95651e9682880842396d6492d19346979f12b61
Abhinav <abhinavkumardps2@gmail.com>
2025-12-14 21:30:13 +0530

    Initial backend setup

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json()); //body-reader
+
+app.post(function(req, res){
+    
+})
+
+app.get(function(req, res){
+    
+})
+
+app.delete(function(req, res){
+    
+})
+
+app.listen(3000);
